829c7e52686e1236c49c3dc54c0c1861918c76d3
gaku1114 <ga1114ku@gmail.com>
2023-03-21 01:01:00 +0900

    Lesson17課題

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -56,16 +56,27 @@ public class UserController {
 
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
-    public String getUser(@PathVariable("id") Integer id, Model model) {
-        // Modelに登録
-        model.addAttribute("user", service.getUser(id));
-        // User更新画面に遷移
-        return "user/update";
+    public String getUser(@PathVariable("id") Integer id, Model model, User user) {
+        if(id == null) {
+            // Modelに登録
+            model.addAttribute(user);
+            // User更新画面に遷移
+            return "user/update";
+        }else {
+            // Modelに登録
+            model.addAttribute("user", service.getUser(id));
+            // User更新画面に遷移
+            return "user/update";
+        }
     }
 
     /** User更新処理 */
     @PostMapping("/update/{id}/")
-    public String postUser(User user) {
+    public String postUser(@Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+            // エラーあり
+            return getUser(null, model, user);
+        }
         // User登録
         service.saveUser(user);
         // 一連画面にリダイレクト
--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -7,6 +7,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PreRemove;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
@@ -15,6 +17,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.Data;
 
@@ -55,5 +58,18 @@ public class User {
     @Email
     @Length(max=50)
     private String email;
+
+    @OneToOne(mappedBy="user")
+    private Authentication authentication;
+
+    /** レコードが削除される前に行なう処理 */
+    @PreRemove
+    @Transactional
+    private void preRemove() {
+        // 認証エンティティからuserを切り離す
+        if (authentication!=null) {
+            authentication.setUser(null);
+        }
+    }
 
 }
